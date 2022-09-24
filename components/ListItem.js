3ce06685923c83c2629fa792bc0d478879321d99
tamaspalya <tamas.palya01@gmail.com>
2022-09-24 16:34:28 +0200

    List item further styled

--- components/ListItem.js
+++ components/ListItem.js
@@ -2,6 +2,9 @@ import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
 import React from 'react'
 
 const ListItem = ({name, sybmol, currentPrice, priceChangePercentage, logoUrl}) => {
+    
+    const priceChangeColor = priceChangePercentage > 0 ? '#34C759' : '#FF3B30';
+
     return (
         <TouchableOpacity>
             <View style={styles.itemWrapper}>
@@ -14,14 +17,14 @@ const ListItem = ({name, sybmol, currentPrice, priceChangePercentage, logoUrl}) 
                     />
                     <View style={styles.titlesWrapper}>
                         <Text style={styles.title}>{name}</Text>
-                        <Text style={styles.subtitle}>{sybmol}</Text>
+                        <Text style={styles.subtitle}>{sybmol.toUpperCase()}</Text>
                     </View>
                 </View>
 
                 {/*Right side*/}
                 <View style={styles.rightWrapper}>
-                    <Text style={styles.title}>{currentPrice}</Text>
-                    <Text style={[styles.subtitle, { color: 'red' }]}>{priceChangePercentage}</Text>
+                    <Text style={styles.title}>${currentPrice}</Text>
+                    <Text style={[styles.subtitle, { color: priceChangeColor }]}>{priceChangePercentage.toFixed(2)}%</Text>
                 </View>
             </View>
         </TouchableOpacity>
